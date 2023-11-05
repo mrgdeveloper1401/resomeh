2278e5c852303e1a6f5f326c41bd0f4ae1adfbd6
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-11-05 11:40:36 +0330

    fix  base permission

--- api/client/permission.py
+++ api/client/permission.py
@@ -3,9 +3,7 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.user == request.user:
-            return True
-            
+        return obj.user == request.user
+
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
+        return request.user and request.user.is_authenticated
