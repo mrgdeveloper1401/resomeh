e2fb33974ab15e80647ce264b30dae07edd87141
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-11-05 11:01:11 +0330

    add permission IsAuthenticated for view client

--- CV/urls.py
+++ CV/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
@@ -28,8 +28,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # jwt token
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/generate/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     
     # swagger
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
--- api/client/views.py
+++ api/client/views.py
@@ -3,6 +3,7 @@ from api.client.serializers import UserChangePasswordSerializer, AboutMeSerializ
 from api.client.serializers import SciolSerializers, EducationSerializers, ExprienceWorkSerializers
 from api.client.serializers import ProjectSerializers, AwardsSerilizers, BookArticleSerilizers, ContactUsSerializers
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 from accounts.models import User
 from home.models import AboutMeModels, SkillModel, EducationModel, ExpreienceWorkModel, ContactUsModel
 from home.models import ProjectModel, AwardsModel, BoookArticleModel, SciolModel, AuthoreModel
@@ -14,111 +15,129 @@ class UserRegisterApiView(CreateAPIView):
     queryset = User.objects.all()
 
 
+
 class UserProfileApiView(RetrieveUpdateDestroyAPIView):
     serializer_class = UserProfileSerializer
     queryset = User.objects.all()
+    permission_classes = (IsAuthenticated, )
     
 
 class UserChangePasswordApiView(UpdateAPIView):
     serializer_class = UserChangePasswordSerializer
     queryset = User.objects.all()
+    permission_classes = (IsAuthenticated, )
    
    
 class AboutMeApiView(CreateAPIView):
     queryset = AboutMeModels.objects.all()
     serializer_class = AboutMeSerializers
+    permission_classes = (IsAuthenticated, )
     
     
 class AboutMeUpdateRetieveDeleteApiView(RetrieveUpdateDestroyAPIView):
     queryset = AboutMeModels.objects.all()
     serializer_class = AboutMeSerializers
+    permission_classes = (IsAuthenticated, )
 
 
 class SkillCreateApiView(CreateAPIView):
     queryset = SkillModel.objects.all()
     serializer_class = SkillSerializers
-    
+    permission_classes = (IsAuthenticated, )
     
 class SkillUpdateRetrieveDeleteApiView(RetrieveUpdateDestroyAPIView):
     queryset = SkillModel.objects.all()
     serializer_class = SkillSerializers
-    
+    permission_classes = (IsAuthenticated, )
 
 class SciolCreateApiView(CreateAPIView):
     queryset = SciolModel.objects.all()
     serializer_class = SciolSerializers
-    
+    permission_classes = (IsAuthenticated, )
 
 class SciolUpdateRetriveDeleteApiView(RetrieveUpdateDestroyAPIView):
     queryset = SciolModel.objects.all()
     serializer_class = SciolSerializers
+    permission_classes = (IsAuthenticated, )
     
 
 class EducatioCreareApiView(CreateAPIView):
     queryset = EducationModel.objects.all()
     serializer_class = EducationSerializers
-    
+    permission_classes = (IsAuthenticated, )
 
 class EducatioUpdateDeleteRetiveApiView(RetrieveUpdateDestroyAPIView):
     queryset = EducationModel.objects.all()
     serializer_class = EducationSerializers
-    
+    permission_classes = (IsAuthenticated, )
 
 class ExprienceCreateApiView(CreateAPIView):
     queryset = ExpreienceWorkModel
     serializer_class = ExprienceWorkSerializers
+    permission_classes = (IsAuthenticated, )
+
     
 
 class ExpriencRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = ExpreienceWorkModel
     serializer_class = ExprienceWorkSerializers
+    permission_classes = (IsAuthenticated, )
     
 
 class ExprienceUpdateDeleteDestroyApiView(RetrieveUpdateDestroyAPIView):
     queryset = ExpreienceWorkModel
     serializer_class = ExprienceWorkSerializers
+    permission_classes = (IsAuthenticated, )
     
 
 class ProjectCreateApiView(CreateAPIView):
     queryset = ProjectModel.objects.all()
     serializer_class = ProjectSerializers
-    
+    permission_classes = (IsAuthenticated, )
 
 class ProjectUpdateRetriveDestroyApiView(RetrieveUpdateDestroyAPIView):
     queryset = ProjectModel.objects.all()
     serializer_class = ProjectSerializers
+    permission_classes = (IsAuthenticated, )
     
     
 class AwardsCreateApiView(CreateAPIView):
     queryset = AwardsModel.objects.all()
     serializer_class = AwardsSerilizers
+    permission_classes = (IsAuthenticated, )
     
     
 class AwardsRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = AwardsModel.objects.all()
     serializer_class = AwardsSerilizers
+    permission_classes = (IsAuthenticated, )
     
     
 class AuthoreCreateAPIView(CreateAPIView):
     queryset = AuthoreModel.objects.all()
     serializer_class = AwardsSerilizers
+    permission_classes = (IsAuthenticated, )
     
 
 class AuthoreRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = AuthoreModel.objects.all()
     serializer_class = AwardsSerilizers
+    permission_classes = (IsAuthenticated, )
     
     
 class BookArticleCreateApiView(CreateAPIView):
     queryset = BoookArticleModel.objects.all()
     serializer_class = BookArticleSerilizers
+    permission_classes = (IsAuthenticated, )
     
 
 class BookArticleRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = BoookArticleModel.objects.all()
     serializer_class = BookArticleSerilizers
+    permission_classes = (IsAuthenticated, )
 
 
 class ContactUsCreateApiview(CreateAPIView):
     queryset = ContactUsModel.objects.all()
     serializer_class = ContactUsSerializers
+    permission_classes = (IsAuthenticated, )
